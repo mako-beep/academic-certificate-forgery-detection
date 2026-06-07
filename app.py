c4f2d26a8251fb0dc48e0bead215a6f587e2574d
Iman Heliliah <emanhelizul@gmail.com>
2026-06-07 21:40:54 +0800

    Connect to Aiven MySQL

--- app.py
+++ app.py
@@ -82,10 +82,12 @@ def allowed_file(filename):
 # =========================
 # MYSQL CONFIG
 # =========================
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'fyproject'
+
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', 12099))
 
 mysql = MySQL(app)
 
