ed0f006c4189d01c3b7a909e6b2318adc95b77c2
Iman Heliliah <emanhelizul@gmail.com>
2026-06-07 21:57:08 +0800

    Debug mysql config

--- app.py
+++ app.py
@@ -82,7 +82,6 @@ def allowed_file(filename):
 # =========================
 # MYSQL CONFIG
 # =========================
-
 app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
